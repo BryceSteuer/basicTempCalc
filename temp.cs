6dd618f05bd09cb3dc8a9bfe8f9b39b905cf7e0a
Bryce Steuer <48736452+BryceSteuer@users.noreply.github.com>
2021-07-27 13:23:32 -0600

    Workable and running version of temp.cs

--- temp.cs
+++ temp.cs
@@ -1,45 +1,46 @@
 using System;
 
-namespace celsiusConversion
+namespace basicTempCalc
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
 
-        // User inputs degrees Celsius
-        Console.WriteLine("What is the current temperature in degrees Celsius?");
 
-        // Take the degree value and store it as a string
-        string degrees = Console.ReadLine();
 
-        // Convert the degrees string to a mathematically manipulatable double
-        Convert.ToDouble(degrees);
+            // User inputs degrees Celsius
+            Console.WriteLine("What is the current temperature in degrees Celsius?");
 
-        // Convert the celsius double to a Fahrenheit double
-        double degreesFahrenheit = (degrees * 9/5) + 32;
+            // Take the degree value and store it as a string
+            string degrees = Console.ReadLine();
 
-        // Print Fahrenheit output
-        Console.WriteLine($"The temperature, therefore, is {degreesFahrenheit} F");
-          
-          // May switch to a switch statement soon
-          if (degreesFahrenheit < 45)
-          {
-            Console.WriteLine("Cold today, is it not?");
-          }
-          else if (degreesFahrenheit > 45 && degreesFahrenheit < 75)
-          {
-            Console.WriteLine("Pretty mild today. Be sure to wear a sweater outside.");
-          }
-          else if (degreesFahrenheit > 75)
-          {
-            Console.WriteLine("Very warm today. Avoid overexertion if possible!");
-          }
-          else 
-          {
-             Console.WriteLine("Enjoy your day!");
-          }
-    }                  
+            // Convert the degrees string to a mathematically manipulatable double
+            int celsiusDouble = Convert.ToInt32(degrees);
+
+            // Convert the celsius decimal to a Fahrenheit double
+            int degreesFahrenheit = (celsiusDouble * 9 / 5) + 32;
+
+            // Print Fahrenheit output
+            Console.WriteLine($"The temperature, therefore, is {degreesFahrenheit} F");
+
+            // May switch to a switch statement soon
+            if (degreesFahrenheit < 45)
+            {
+                Console.WriteLine("Cold today, is it not?");
+            }
+            else if (degreesFahrenheit > 45 && degreesFahrenheit < 75)
+            {
+                Console.WriteLine("Pretty mild today. Be sure to wear a sweater outside.");
+            }
+            else if (degreesFahrenheit > 75)
+            {
+                Console.WriteLine("Very warm today. Avoid overexertion if possible!");
+            }
+            else
+            {
+                Console.WriteLine("Enjoy your day!");
+            }
+        }
     }
 }
-
