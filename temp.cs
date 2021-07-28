b091aad97cb54db5d0cf9e44897045471c1dd67d
Bryce Steuer <48736452+BryceSteuer@users.noreply.github.com>
2021-07-28 12:06:10 -0600

    Update temp.cs

--- temp.cs
+++ temp.cs
@@ -46,6 +46,41 @@ namespace basicTempCalc
                     Console.WriteLine("Enjoy your day!");
                 }
             }
+            else if (celOrFar == "Celsius")
+            {
+                // User inputs degrees Fahrenheit
+                Console.WriteLine("What is the current temperature in degrees Fahrenheit?");
+
+                // Take the degree value and store it as a string
+                string degreesFahr = Console.ReadLine();
+
+                // Convert the degrees string to a mathematically manipulatable double
+                int fahrDouble = Convert.ToInt32(degreesFahr);
+
+                // Convert the celsius decimal to a Fahrenheit double
+                int degreesCelsius = (fahrDouble - 32) * 5/9;
+
+                // Print Fahrenheit output
+                Console.WriteLine($"The temperature, therefore, is {degreesCelsius} C");
+
+                // May switch to a switch statement soon
+                if (degreesCelsius < 14)
+                {
+                    Console.WriteLine("Cold today, is it not?");
+                }
+                else if (degreesCelsius > 14 && degreesCelsius < 22)
+                {
+                    Console.WriteLine("Pretty mild today. Be sure to wear a sweater outside.");
+                }
+                else if (degreesCelsius > 22)
+                {
+                    Console.WriteLine("Very warm today. Avoid overexertion if possible!");
+                }
+                else
+                {
+                    Console.WriteLine("Enjoy your day!");
+                }
+            }
         }
     }
 }
